3236f788236dfea6e05a098bdb0a2e67d4d527a2
kripersi <kripersi1123@gmail.com>
2025-11-19 17:28:32 +0300

    Добавил базовую регистрацию, вход, профиль

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,7 @@ app_name = 'users'
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='users:login'), name='logout'),
     path('profile/', views.profile, name='profile'),
     path('', views.users_home, name='users_home'),
 ]
--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, ProfileUpdateForm
+from django.contrib import messages
 
 
 def users_home(request):
@@ -13,10 +14,12 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
+            messages.success(request, "Аккаунт создан!")
             login(request, user)
-            return redirect('users:profile')
+            return redirect('core:home')
     else:
         form = UserRegisterForm()
+
     return render(request, 'users/register.html', {'form': form})
 
 
