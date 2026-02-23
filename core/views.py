cf0224f8a9b8c0ab37b4713123261f5b3b82aa11
kripersi <kripersi1123@gmail.com>
2026-02-23 21:11:47 +0300

    Добавил фильтр по странам

--- core/views.py
+++ core/views.py
@@ -3,10 +3,12 @@ from posts.models import Post
 from django.db.models import Q
 from django.contrib.postgres.search import TrigramSimilarity
 from django.db.models.functions import Greatest
+from posts.forms import COUNTRIES
 
 
 def home(request):
     query = request.GET.get("q")
+    selected_country = request.GET.get("country")
     posts = Post.objects.all().order_by("-created_at")
 
     if query:
@@ -26,8 +28,13 @@ def home(request):
             .distinct()
         )
 
+    if selected_country:
+        posts = posts.filter(country=selected_country)
+
     return render(request, "core/home.html", {
         "posts": posts,
-        "query": query
+        "query": query,
+        "countries": [country[0] for country in COUNTRIES],
+        "selected_country": selected_country,
     })
 
